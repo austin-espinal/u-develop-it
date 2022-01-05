53a7c35c79e99b9c0767346e74c0310bd263109d
austin espinal <austinespi@gmail.com>
2022-01-05 17:21:27 -0500

    updated GET candidates routes

--- server.js
+++ server.js
@@ -21,7 +21,11 @@ const db = mysql.createConnection(
 
 //GET all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id`;
 
     db.query(sql, (err, rows) => {
         if (err) {
@@ -38,7 +42,12 @@ app.get('/api/candidates', (req, res) => {
 
 //GET a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id 
+             WHERE candidates.id = ?`;
     const params = [req.params.id];
 
     db.query(sql, params, (err, row) => {
@@ -77,9 +86,9 @@ app.delete('/api/candidate/:id', (req, res) => {
 });
 
 // Create a candidate
-app.post('/api/candidate', ({body}, res) => {
+app.post('/api/candidate', ({ body }, res) => {
     const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
-    if(errors) {
+    if (errors) {
         res.status(400).json({ error: errors });
         return;
     }
@@ -89,7 +98,7 @@ app.post('/api/candidate', ({body}, res) => {
     const params = [body.first_name, body.last_name, body.industry_connected];
 
     db.query(sql, params, (err, result) => {
-        if(err) {
+        if (err) {
             res.status(400).json({ error: err.message });
             return;
         }
