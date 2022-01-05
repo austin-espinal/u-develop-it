35c2c17fdf6fb83a665c765c98c1f9a92416b9bb
austin espinal <austinespi@gmail.com>
2022-01-05 01:32:52 -0500

    add db queries

--- server.js
+++ server.js
@@ -18,15 +18,39 @@ const db = mysql.createConnection(
     console.log('Connecting to the election database.')
 );
 
-db.query(`SELECT * FROM candidates`, (err, rows)=> {
-    console.log(rows);
-});
+//GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, rows)=> {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(rows);
+// });
+
+//Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result)=> {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+//               VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
 
 //default response for any other request (not found)
 app.use((req, res) => {
     res.status(404).end();
 });
 
-app.listen(PORT, ()=> {
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
